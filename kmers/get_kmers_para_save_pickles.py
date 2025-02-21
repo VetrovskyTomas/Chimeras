939483eb105e66a91c5c658fd3e4353556b565e7
Michal Hub <97857298+huubMichelsen@users.noreply.github.com>
2025-02-21 15:41:35 +0100

    Add files via upload

--- kmers/get_kmers_para_save_pickles.py
+++ kmers/get_kmers_para_save_pickles.py
@@ -18,23 +18,30 @@ def extract_kmers(seq, k_size): # this is somehow faster than this: [seq[r:r+k_s
 
 
 def process_chunk(para_input):
-    chunk_dict, k_size = para_input
-    #res_dic = {k:[] for k in chunk_dict.keys()}
-    for k,v in chunk_dict.items():
-        kmers = list(chain.from_iterable([extract_kmers(seq,k_size) for seq in v]))
-        with gzip.open(f'/home/mg/ChimeraProject/pickles/all{k_size}mers/{k}.pickle.gzip', 'wb') as f:
-            pickle.dump({k:kmers}, f, protocol=pickle.HIGHEST_PROTOCOL)
+    k, v, k_size, out_dir = para_input
+    kmers = list(chain.from_iterable([extract_kmers(seq,k_size) for seq in v]))
+    with gzip.open(f'{out_dir}{k}.pickle.gzip', 'wb') as f:
+        pickle.dump({k:kmers}, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-        #if kmer_count_threshold > 1: com_all_kmers = set([kmer for kmer, count in Counter(kmers).items() if count > kmer_count_threshold])
-        #elif kmer_count_threshold == 1: com_all_kmers = set(kmers)
-        #res_dic[k] = com_all_kmers   
-    #return res_dic
-
-def get_kmers_para_pickle(ref_seqs,k_size=50,threads = 1,name_patter = "(f__).+?(;)"): # dont need par_of_fasta_id
+def get_kmers_para_pickle(ref_seqs,k_size=50,threads = 1,name_patter=None,out_dir=None): # dont need par_of_fasta_id
     """
-    Takes list of biopython reads from UNITE database and returns dictionary of kmers present in each family
+    Takes list of biopython reads from UNITE database and saves kmers present in a taxonomic group [genus, family, class, order] in .../all{k_mer_size}.pickle.gzip
     """
     start = time.time()
+    ref_seqs = [i for i in SeqIO.parse(ref_seqs,"fasta")]
+
+    if not name_patter: print("No name pattern!") ; return
+    elif "g" in name_patter.lower(): pat = "g"
+    elif "f" in name_patter.lower(): pat = "f"
+    elif "c" in name_patter.lower(): pat = "c"
+    elif "o" in name_patter.lower(): pat = "o"
+    name_patter = f"({pat}__).+?(;)"
+    
+    if not out_dir : print("No output directory!") ; return
+    if out_dir[-1] != "/": out_dir = out_dir+"/"
+    out_dir = f"{out_dir}all{k_size}mers/"
+    os.makedirs(out_dir, exist_ok=True)
+    
     ref_seqs = [(re.search(name_patter,i.id).group().strip(";"),i.seq) for i in ref_seqs] # makes list of tuple(family-name , sequence) from list of SeqRecords
     names = set([i[0] for i in ref_seqs])
     print(f"{len(ref_seqs)} of sequences, {len(names)} of unique taxa")
@@ -45,16 +52,10 @@ def get_kmers_para_pickle(ref_seqs,k_size=50,threads = 1,name_patter = "(f__).+?
         if "unidentified" in k.lower():
             dic_seqs.pop(k)
             break
-            
-    keys = list(dic_seqs.keys())
-    chunk_size = len(keys) // threads  
-    key_chunks = [keys[i:i + chunk_size] for i in range(0, len(keys), chunk_size)]
-
-    if len(keys) > chunk_size:
-        key_chunks[-1].extend(keys[chunk_size*threads:])
-
-    para_input = [({k:dic_seqs[k] for k in k_c},k_size) for k_c in key_chunks]
+    lenka = len(names)
     with Pool(threads) as pool:
-        results = pool.map(process_chunk, para_input)
-
+        para_input = [(k,v,k_size,out_dir) for k,v in dic_seqs.items()]
+        for i, result in enumerate(pool.imap_unordered(process_chunk, para_input)):
+            if i % 10 == 0: print(f"{i}/{lenka}      ", end="\r")
+                
     print(f"Execution Time: {time.time() - start}")
